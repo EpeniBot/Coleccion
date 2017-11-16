03487076cd7c1ab9016d35ebfff830830ca820c9
EpeniBot <33704759+EpeniBot@users.noreply.github.com>
2017-11-15 20:09:40 -0400

    Update Apostador.js

--- Apostador.js
+++ Apostador.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         EpeniBot / FreeBitcoin Apostador
+// @name         EpeniBot | FreeBitcoin Apostador
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Apostando en Freebitcoin!
